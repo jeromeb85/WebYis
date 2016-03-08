66258dca3ef686e7c5a1017483142ab567e5fd05
jeromeb85 <jerome.boisseau@orange.fr>
2016-03-08 22:57:45 +0100

    new

--- index.php
+++ index.php
@@ -16,37 +16,46 @@
     <!-- Javascript-->
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
+<!--    <script src="js/main.js"></script>-->
+    
     
   </head>
   <body>
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container-fluid">
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container-fluid">   
             <div class="navbar-header">
-                <a class="navbar-brand pull-left" href="#">
-                    <img alt="Brand" src="img/Home32x32.png">
-                </a>
-            </div>
-<!--            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Project name</a>
-            </div>-->
-            <div id="navbar" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav barmenu">
+                <a class="navbar-brand" href="#">HOME</a>                
+            </div>
+            <div id="navbar" class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
                     <li class="active"><a href="#">CLIENTS</a></li>
                     <li><a href="#about">COMMANDES</a></li>
                     <li><a href="#contact">FACTURES</a></li>
                 </ul>
-            </div><!--/.nav-collapse -->
-        </div>
-    </nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Notif</a></li>
+                </ul>                
+            </div>          
+        </div>  
+      </nav>
+      
+      <div class="container-fluid" id="main-container">
+        <div class="flex" id="home">          
+            <div class="item">0</div>
+            <div class="item">1</div>
+            <div class="item">2</div>
+            <div class="item">3</div>
+            <div class="item">4</div>   
+        </div>            
+      </div>
 
-    <div class="container-fluid" id="main-container">
+<!--    <div class="container-fluid" id="main-container">
          <div class="app">
 
         <div class="flex">
@@ -69,7 +78,7 @@
     
          </div>    
  
-    </div>      
+    </div>      -->
 
 
 
